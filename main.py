fc25a48fc25ef0ddbf69b96801bacefae05d8c51
ekaterinasemenova <semenovakat02@gmail.com>
2022-12-11 07:58:57 +0700

    17.9.1 HomeWork

--- main.py
+++ main.py
@@ -1,57 +1,18 @@
-# This is a sample Python script.
+def par_checker(string):
+    stack = []
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    for s in string:
+        if s == "(":
+            stack.append(s)
+        elif s == ")":
+            if len(stack) > 0 and stack[-1] == "(":
+                stack.pop()
+            else:
+                return False
+    return len(stack) == 0
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print(1)
-print(2)
-print (0.1+0.1*5-0.3*4)
-print ((3.14+0.3)/2+0.15)
-print (31 % 2 + 31 % 2)
-print (13 % 3 * 3 - 3**2)
-print(round(11*2.5/3, 2))
-print(round(3.14159 **2/2))
-print('1 2 3 4 5 6 7')
-pi = 31.4159265
-print ("%.4e" % (pi))
-
-d = {'day' : 22, 'month' : 6, 'year' : 2015}
-print("||".join(d.keys()))
-print(1)
-L = ['a', 'b', 'c']
-print(id(L))
-L.append('d')
-print(id(L))
-colors = 'red blue green'
-print(colors.split('red blue green'))
-s = 5
-a = 10
-if a > 0:
-   s = s + a
-else:
-   s = s - a
-
-print(s)
-print(list(range(5)))
-a = "foo"
-b = "bar"
-
-print(1 and a or b)
-a = ""
-b = "bar"
-
-print(1 and a or b)
+print(par_checker("(5+6)*(7+8)/(4+3)"))
 
 
 
