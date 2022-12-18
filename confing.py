844afb389ebf2eb916a7f2877db1394ab287c55b
ekaterinasemenova <semenovakat02@gmail.com>
2022-12-18 18:16:34 +0700

    Итоговый проект

---
+++ confing.py
@@ -0,0 +1,8 @@
+TOKEN = "5986915482:AAGUuS9TMzwWqGDnFQikzexrbhZj8Mgl4OM"
+CURRENCY_API = "7dfba99f329fd57790e6662a96b62d1cd769df513d7e3ae5bc27dbfd0d27e004"
+
+keys = {
+    "евро": "EUR",
+    "доллар": "USD",
+    "рубль": "RUB",
+}
